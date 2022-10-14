fa8c8109aaf04723541af0f8822a6dea5fcf7726
Rob Boorsma <robboorsma@gmail.com>
2022-10-14 15:20:54 +0200

    Push To test SonarQube
    Sonarqube should actually test this

--- src/main/java/anyfly/flightprogram/PasswordHasher.java
+++ src/main/java/anyfly/flightprogram/PasswordHasher.java
@@ -14,6 +14,6 @@ public class PasswordHasher {
     }
     public boolean Authenticate(String Email, String Password)
     {
-        return _encoder.matches(Password, "put method here"); //TODO Fill Metho
+        return _encoder.matches(Password, "put method here"); //TODO Fill Method
     }
 }
